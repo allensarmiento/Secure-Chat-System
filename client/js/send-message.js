669c1f03bb1b8406b140dde570e3007af688aed4
Allen-Sarmiento <allensarmiento3@gmail.com>
2019-04-29 15:22:33 -0700

    Enter key works to send messages

--- client/js/send-message.js
+++ client/js/send-message.js
@@ -2,7 +2,19 @@ function sendMessage() {
   let message = document.getElementById("message").value;
   document.getElementById("message").value = "";
 
-  document.getElementById("messages").innerHTML += 
-    "<p>" + message + "</p>";
+  document.getElementById("chatbox").innerHTML +=
+    "<p class='chatmessage sent'>" + message + "</p>";
+
+  let chatbox = document.getElementById("chatbox");
+  chatbox.scrollTop = chatbox.scrollHeight;
 }
 
+document.onkeypress = keyPress;
+
+function keyPress(e) {
+  var x = e || window.event;
+  var key = (x.keyCode || x.which);
+  if (key == 13 || key == 3) {
+    sendMessage();
+  }
+}
