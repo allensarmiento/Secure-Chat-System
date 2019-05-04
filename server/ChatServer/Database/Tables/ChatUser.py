4906dee91d7d2b281c961363dbbe616f74216819
Nathan-LS <nathan@nathan-s.com>
2019-05-03 23:57:13 -0700

    Working on passwords for initial auth.

--- server/ChatServer/Database/Tables/ChatUser.py
+++ server/ChatServer/Database/Tables/ChatUser.py
@@ -1,7 +1,18 @@
 from ChatServer.Database.Tables import DeclarativeBase
-from sqlalchemy import Column, Integer, String, LargeBinary
+from sqlalchemy import Column, Integer, String, LargeBinary, Binary
 import bcrypt
 from OpenSSL import crypto
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.serialization import load_pem_public_key
+from ChatServer.Database import DatabaseManager
+import asyncio
+from functools import partial
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import utils
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
 
 
 class ChatUser(DeclarativeBase.Base):
@@ -9,8 +20,8 @@ class ChatUser(DeclarativeBase.Base):
 
     user_id = Column(Integer, primary_key=True, nullable=False, autoincrement=False)
     user_name = Column(String, default="", nullable=False, index=True)
-    user_password = Column(String, nullable=False)
-    user_public_key = Column(LargeBinary, nullable=False)
+    user_password = Column(Binary, nullable=False)
+    user_public_key = Column(Binary, nullable=False)
 
     def __init__(self, user_id: int, user_name: str, user_pass: str):
         self.user_id = user_id
@@ -29,10 +40,28 @@ class ChatUser(DeclarativeBase.Base):
     def get_name(self) -> str:
         return self.user_name
 
-    def test_password(self, input_password: str):
+    def test_password(self, input_password_encrypted: str):
         """Returns true if the user password matches db pass, else false"""
-        return bcrypt.checkpw(input_password.encode('utf-8'), self.user_password)
+        return bcrypt.checkpw(input_password_encrypted.encode('utf-8'), self.encrypt_string(self.user_password))
+
+    def encrypt_string(self, decrypted_string):
+        key = serialization.load_pem_public_key(self.user_public_key, backend=default_backend())
+        return key.encrypt(decrypted_string, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA3_256()),
+                                                       algorithm=hashes.SHA3_256(),
+                                                       label=None))
 
     def __str__(self):
         return "ID: {} Name: {}".format(self.user_id, self.user_name)
+
+    @classmethod
+    def _get_user(cls, user_id):
+        db = DatabaseManager.DatabaseManager.get_session()
+        try:
+            return db.query(cls).filter(cls.user_id == user_id).one_or_none()
+        finally:
+            db.close()
+
+    @classmethod
+    async def get_user(cls, user_id):
+        return await asyncio.get_event_loop().run_in_executor(None, partial(cls._get_user, user_id))
 
